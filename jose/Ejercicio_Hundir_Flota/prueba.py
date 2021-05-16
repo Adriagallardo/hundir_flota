8cade73c945a1c8047c24d2ed1aa27ef8804bd14
Adrià Gallardo <82371848+Adriagallardo@users.noreply.github.com>
2021-05-16 18:09:19 +0200

    Delete jose/Ejercicio_Hundir_Flota directory

--- jose/Ejercicio_Hundir_Flota/prueba.py
+++
@@ -1,4 +0,0 @@
-import json
-guardar_partida(dict_posciones, datos_enemigos):
-    with open('Partidas_guardadas/partidas.json', 'a+') as partida:
-        json.dump([dict_posiciones, datos_enemigo], partida, indent=4)
